c0b0db516f48e6dc38962bd9ba39726a4d21dd4a
Leewi <wilee.bm@gmail.com>
2026-08-29 21:31:19 -0500

    asdasd

---
+++ AlmacenWeb/src/componentes/encabezado.jsx
@@ -0,0 +1,19 @@
+function encabezado() {
+  return (
+    <header className='encabezado-contenedor'>
+
+      <div className='encabezado-logo'>
+
+        <link to='/'>
+
+          <img src='' alt='' />
+
+        </link>
+
+      </div>
+      
+    </header>
+  );
+}
+
+export default encabezado;
---
+++ AlmacenWeb/src/paginas/pagina_inicio.jsx
@@ -0,0 +1,22 @@
+import pagina_inicio from 'react'
+
+function pagina_inicio() {
+  return (
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Pagina Inicio</title>
+    </head>
+    <body>
+
+        <header>
+            
+        </header>
+
+    </body>
+    </html>
+  )
+}
+
+export default pagina_inicio

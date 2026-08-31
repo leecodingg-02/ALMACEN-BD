f4b7530d126bb972b7ada3fa873722d5d85a4470
Leewi <wilee.bm@gmail.com>
2026-08-30 22:40:02 -0500

    s

--- AlmacenWeb/src/App.jsx
+++ AlmacenWeb/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import { Encabezado } from './componentes/encabezado'
-
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Encabezado from './componentes/encabezado'
+import Pagina_inicio from './paginas/pagina_inicio'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Encabezado/>
-    <Routes>
-      <Route path="/" element={<Pagina_inicio />} />
-    </Routes>
+      <Encabezado />
+      <Routes>
+        <Route path="/" element={<Pagina_inicio />} />
+      </Routes>
     </>
   )
 }
--- AlmacenWeb/src/paginas/pagina_inicio.jsx
+++ AlmacenWeb/src/paginas/pagina_inicio.jsx
@@ -2,17 +2,10 @@ import { Link, useNavigate } from "react-router-dom";
 
 function Pagina_inicio() {
   return (
-    <html lang="es">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Pagina Inicio</title>
-    </head>
-    <body>
+    <main>
       <h1>HOLA MUNDO</h1>
 
-    </body>
-    </html>
+    </main>
   )
 }
 

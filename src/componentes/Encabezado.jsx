e5513d59584cc02eab7aa3a2aa9e575bfa84dd1f
Leewi <wilee.bm@gmail.com>
2026-09-02 11:17:26 -0500

    asdasdasd

--- src/componentes/Encabezado.jsx
+++ src/componentes/Encabezado.jsx
@@ -125,7 +125,29 @@ function Encabezado() {
             Inicio{" "}
           </NavLink>
 
-          <button className='btn-productos'> Productos </button>
+          <div className='btn-desplegable'>
+            <button className='btn-productos'>
+
+              {" "}
+              Productos
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke-width='1.5'
+                stroke='currentColor'
+                class='size-6'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='m19.5 8.25-7.5 7.5-7.5-7.5'
+                />
+              </svg>
+
+            </button>
+            
+          </div>
 
           <NavLink to='/ofertas' className='navegacion'>
             {" "}
